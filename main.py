c55e2afee0ad59c4a32f068ebef2f382dd534b38
Kenneth Lin <70595901+246971@users.noreply.github.com>
2022-02-09 22:54:11 -0500

    new thingy 2/9/2022 10:54 pm

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-import cv2
+# importing the necessary libraries
+from cv2 import cv2
 import numpy as np
 
-video = cv2.VideoCapture("/Users/local/PycharmProjects/rollingcan_kennethlin/IMG_9653.mov")
+# Creating a VideoCapture object to read the video
+cap = cv2.VideoCapture(r"C:\Users\klin1\Downloads\rolling_can.mov")
 
-while video.isOpened():
-    frame = video.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+while cap.isOpened():
 
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
 
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-            cv2.circle(frame, (x, y), 5, (0, 128, 255), -1)
-        cv2.imshow("frame", frame)
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            for (x, y, r) in circles:
+                cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+                cv2.circle(frame, (x, y), 5, (0, 128, 255), -1)
+            cv2.imshow("frame", frame)
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
+        break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
