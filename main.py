5cb05b548937babfb156f9f9ad0c55181daaf6f8
Kenneth Lin <70595901+246971@users.noreply.github.com>
2022-02-10 11:36:30 -0500

    pwp 4th period 11:36 2/10/2022

--- main.py
+++ main.py
@@ -3,30 +3,38 @@ from cv2 import cv2
 import numpy as np
 
 # Creating a VideoCapture object to read the video
-cap = cv2.VideoCapture(r"C:\Users\klin1\Downloads\rolling_can.mov")
+# cap = cv2.VideoCapture(r"C:\Users\klin1\Downloads\rolling_can.mov")
+cap = cv2.VideoCapture("/Users/local/Downloads/rolling_can.mov")
 
-while cap.isOpened():
+while(1):
 
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+    frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if circles is not None:
-            circles = np.round(circles[0, :]).astype("int")
-            for (x, y, r) in circles:
-                cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-                cv2.circle(frame, (x, y), 5, (0, 128, 255), -1)
-            cv2.imshow("frame", frame)
+    gray_blurred = cv2.blur(gray, (3, 3))
 
-        # Press Q on keyboard to  exit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+    detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=1, maxRadius=40)
 
-    # Break the loop
-    else:
+    # Draw circles that are detected.
+    if detected_circles is not None:
+
+        # Convert the circle parameters a, b and r to integers.
+        detected_circles = np.uint16(np.around(detected_circles))
+
+        for pt in detected_circles[0, :]:
+            a, b, r = pt[0], pt[1], pt[2]
+
+            # Draw the circumference of the circle.
+            cv2.circle(frame, (a, b), r, (0, 255, 0), 2)
+
+            # Draw a small circle (of radius 1) to show the center.
+            cv2.circle(frame, (a, b), 1, (0, 0, 255), 3)
+            cv2.imshow("Detected Circle", frame)
+
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
         break
+
 
 # When everything done, release the video capture object
 cap.release()
