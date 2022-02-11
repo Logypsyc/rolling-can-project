4554d4374a0a22ac24e1d1e29ba387d8266c9c05
Kenneth Lin <70595901+246971@users.noreply.github.com>
2022-02-11 11:41:19 -0500

    2/11/2022 11:41am

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-from cv2 import cv2
+import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(r"rolling_can.mov")
-while 1:
+# cap = cv2.VideoCapture(r"rolling_can.mov")
+cap = cv2.VideoCapture("/Users/local/PycharmProjects/rollingcan_kennethlin/rolling_can.mov")
+
+totalFrames = 0
+countedFrames = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+while cap.isOpened():
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    totalFrames += 1
+    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray_blurred = cv2.GaussianBlur(gray, (11, 11), 0)
-    detected_circles = cv2.HoughCircles(gray_blurred,
-                                        cv2.HOUGH_GRADIENT, 1, 20, param1=50,
-                                        param2=30, minRadius=1, maxRadius=40)
+        gray_blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+        detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=1, maxRadius=40)
 
-    if detected_circles is not None:
+        if detected_circles is not None:
 
-        detected_circles = np.uint16(np.around(detected_circles))
+            detected_circles = np.uint16(np.around(detected_circles))
 
-        for pt in detected_circles[0, :]:
-            a, b, r = pt[0], pt[1], pt[2]
+            for pt in detected_circles[0, :]:
+                a, b, r = pt[0], pt[1], pt[2]
 
-            cv2.circle(frame, (a, b), r, (0, 255, 0), 2)
+                cv2.circle(frame, (a, b), r, (0, 255, 0), 2)
+                cv2.circle(frame, (a, b), 1, (0, 0, 255), 3)
 
-            cv2.circle(frame, (a, b), 1, (0, 0, 255), 3)
-            cv2.imshow("Detected Circles", frame)
+            countedFrames += 1
 
-    k = cv2.waitKey(30) & 0xff
-    # press the space bar to break the loop
-    if k == 32:
+        cv2.putText(frame, 'total frames: ' + str(totalFrames), (50, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'counted frames: ' + str(countedFrames), (50, 25), font, 1, (0, 255, 255), 2, cv2.LINE_4)
+
+        cv2.imshow("Detected Circles", frame)
+        k = cv2.waitKey(1) & 0xff
+    else:
         break
+
+cv2.waitKey(0)
 
 cv2.destroyAllWindows()
 cap.release()
