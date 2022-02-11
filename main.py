767f66d7651a3355630778f0a3a5b645d6c577da
Kenneth Lin <70595901+246971@users.noreply.github.com>
2022-02-11 01:34:12 -0500

    changes made at 1:33AM 2/11/2022
    the can now actually gets detected

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
-# importing the necessary libraries
 from cv2 import cv2
 import numpy as np
 
-# Creating a VideoCapture object to read the video
-# cap = cv2.VideoCapture(r"C:\Users\klin1\Downloads\rolling_can.mov")
-cap = cv2.VideoCapture("/Users/local/Downloads/rolling_can.mov")
-
-while(1):
-
-    frame = cap.read()
+cap = cv2.VideoCapture(r"rolling_can.mov")
+while 1:
+    ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray_blurred = cv2.blur(gray, (3, 3))
+    gray_blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+    detected_circles = cv2.HoughCircles(gray_blurred,
+                                        cv2.HOUGH_GRADIENT, 1, 20, param1=50,
+                                        param2=30, minRadius=1, maxRadius=40)
 
-    detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=1, maxRadius=40)
-
-    # Draw circles that are detected.
     if detected_circles is not None:
 
-        # Convert the circle parameters a, b and r to integers.
         detected_circles = np.uint16(np.around(detected_circles))
 
         for pt in detected_circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
 
-            # Draw the circumference of the circle.
             cv2.circle(frame, (a, b), r, (0, 255, 0), 2)
 
-            # Draw a small circle (of radius 1) to show the center.
             cv2.circle(frame, (a, b), 1, (0, 0, 255), 3)
-            cv2.imshow("Detected Circle", frame)
+            cv2.imshow("Detected Circles", frame)
 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    k = cv2.waitKey(30) & 0xff
+    # press the space bar to break the loop
+    if k == 32:
         break
 
-
-# When everything done, release the video capture object
-cap.release()
-
-# Closes all the frames
 cv2.destroyAllWindows()
+cap.release()
