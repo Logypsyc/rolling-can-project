aeb6572407d58381277ef0422cee53265016a525
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-02-28 11:44:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,38 +12,3 @@ imgOutput = cv2.warpPerspective(img, matrix, (width, height))
 
 cv2.imshow("output image", imgOutput)
 cv2.waitKey(0)
-
-
-# function to display the coordinates of
-# of the points clicked on the image
-def click_event(event, x, y, flags, params):
-    # checking for left mouse clicks
-    if event == cv2.EVENT_LBUTTONDOWN:
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)
-
-        # displaying the coordinates
-        # on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(image, str(x) + ',' + str(y), (x, y), font, 1, (0, 0, 255), 2)
-        cv2.imshow('image', image)
-
-
-# driver function
-if __name__ == "__main__":
-    # reading the image
-    image = cv2.imread("/Users/local/PycharmProjects/findcircles_kennethlin/test_l3.jpg")
-
-    # displaying the image
-    cv2.imshow('image', image)
-
-    # setting mouse handler for the image
-    # and calling the click_event() function
-    cv2.setMouseCallback('image', click_event)
-
-    # wait for a key to be pressed to exit
-    cv2.waitKey(0)
-
-    # close the window
-    cv2.destroyAllWindows()
